578fe4dedf790275081ec669c77b68de1f0194ac
fredrik.skogberg <fredrik.skogberg@workday.com>
2019-12-04 22:36:10 +0000

    Cleanup solution for day 4

--- src/main/scala/freskog/day4/SolutionDay4.scala
+++ src/main/scala/freskog/day4/SolutionDay4.scala
@@ -2,62 +2,47 @@ package freskog.day4
 
 import zio._
 import zio.console.Console
+import fastparse._, NoWhitespace._
 
 object SolutionDay4 extends App {
-
-  val pairPattern = """(\d)(\d)""".r
-
-  def nonDecreasing(numeric: String): Boolean =
-    numeric.toSeq.sliding(2).map(_.toString).forall {
-      case pairPattern(d1, d2) => d2.toInt >= d1.toInt
-    }
-
-  def negate(bool: Boolean): Boolean = !bool
-
-  def hasAtLeastOneDouble(numeric: String): Boolean =
-    negate(numeric.toSeq.sliding(2).map(_.toString()).forall {
-      case pairPattern(d1, d2) => d1 != d2
-    })
-
-  def inRange(from: Int, to: Int): Iterator[String] =
-    Iterator
-      .from(from)
-      .take(to - from)
-      .map(_.toString)
-      .filter(
-        numeric => nonDecreasing(numeric) && hasAtLeastOneDouble(numeric)
-      )
-
-  val partOne: ZIO[Console, Nothing, Int] =
-    ZIO
-      .succeed(inRange(153517, 630395).size)
-      .flatMap(
-        n => console.putStrLn(s"$n digits in range 153517, 630395")
-      )
-      .as(0)
-
-  import fastparse._, NoWhitespace._
 
   def number[_: P]: P[Int]                = P(CharPred(_.isDigit).!.map(_.toInt))
   def equalTo[_: P](n: Int): P[Int]       = P(number.filter(_ == n))
   def greaterThan[_: P](n: Int): P[Int]   = P(number.filter(_ > n))
-  def greaterOrEqTo[_: P](n: Int): P[Int] = number.filter(_ >= n)
+  def greaterOrEqTo[_: P](n: Int): P[Int] = P(number.filter(_ >= n))
 
-  def aPairGreaterThan[_: P](n: Int): P[Int] =
+  def digitRepeatedExactly2[_: P](n: Int): P[Int] =
     P(greaterThan(n).flatMapX(n => equalTo(n) ~ &(!equalTo(n) | End)))
 
-  def nonDecreasingSeq[_: P](n: Int): P[Int] =
-    P(greaterOrEqTo(n).flatMapX(nonDecreasingSeq) | greaterOrEqTo(n) ~ End | End ~ Pass(n))
+  def digitRepeatedAtLeast2[_: P](n: Int): P[Int] =
+    P(greaterThan(n).flatMapX(n => equalTo(n)))
 
-  def onlyStrictPairs[_: P](prev: Int): P[Int] =
-    P(greaterOrEqTo(prev).flatMapX(onlyStrictPairs) | aPairGreaterThan(prev).flatMapX(nonDecreasingSeq))
+  def zeroOrMoreNonDecreasing[_: P](n: Int): P[Int] =
+    P(greaterOrEqTo(n).flatMapX(zeroOrMoreNonDecreasing) | greaterOrEqTo(n) ~ End | End ~ Pass(n))
+
+  def partOneParser[_: P](n: Int): P[Int] =
+    P(greaterOrEqTo(n).flatMapX(partOneParser) | digitRepeatedAtLeast2(n).flatMapX(zeroOrMoreNonDecreasing))
+
+  def partTwoParser[_: P](n: Int): P[Int] =
+    P(greaterOrEqTo(n).flatMapX(partTwoParser) | digitRepeatedExactly2(n).flatMapX(zeroOrMoreNonDecreasing))
+  
+  val partOne: ZIO[Console, Nothing, Unit] =
+    console
+      .putStrLn(s"""${Iterator
+        .from(153517)
+        .take(630395 - 153517)
+        .map(_.toString)
+        .count(n => parse(n, partOneParser(0)(_)).isSuccess)} valid numbers in range""")
+      .as(0)
 
   val partTwo: ZIO[Console, Nothing, Int] =
-    console.putStrLn(s"""${Iterator
-      .from(153517)
-      .take(630395 - 153517)
-      .map(_.toString)
-      .count(n => parse(n, onlyStrictPairs(0)(_)).isSuccess)} valid numbers in range""").as(0)
+    console
+      .putStrLn(s"""${Iterator
+        .from(153517)
+        .take(630395 - 153517)
+        .map(_.toString)
+        .count(n => parse(n, partTwoParser(0)(_)).isSuccess)} valid numbers in range""")
+      .as(0)
 
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
     partOne *> partTwo
--- src/test/scala/freskog/day4/SolutionDay4Spec.scala
+++ src/test/scala/freskog/day4/SolutionDay4Spec.scala
@@ -10,13 +10,13 @@ object SolutionDay4Spec extends DefaultRunnableSpec({
 
 
   suite("PartTwo")(
-    test("112233 ok")(assert(parse("112233",onlyStrictPairs(0)(_)).isSuccess, Assertion.equalTo(true))),
-    test("112230 fail")(assert(parse("112230",onlyStrictPairs(0)(_)).isSuccess, Assertion.equalTo(false))),
-    test("122223 fail")(assert(parse("122223",onlyStrictPairs(0)(_)).isSuccess, Assertion.equalTo(false))),
-    test("112345 ok")(assert(parse("112345",onlyStrictPairs(0)(_)).isSuccess, Assertion.equalTo(true))),
-    test("102230 fail")(assert(parse("012230",onlyStrictPairs(0)(_)).isSuccess, Assertion.equalTo(false))),
-    test("123444 fail")(assert(parse("123444",onlyStrictPairs(0)(_)).isSuccess, Assertion.equalTo(false))),
-    test("111122 ok")(assert(parse("111122",onlyStrictPairs(0)(_)).isSuccess, Assertion.equalTo(true)))
+    test("112233 ok")(assert(parse("112233",partTwoParser(0)(_)).isSuccess, Assertion.equalTo(true))),
+    test("112230 fail")(assert(parse("112230",partTwoParser(0)(_)).isSuccess, Assertion.equalTo(false))),
+    test("122223 fail")(assert(parse("122223",partTwoParser(0)(_)).isSuccess, Assertion.equalTo(false))),
+    test("112345 ok")(assert(parse("112345",partTwoParser(0)(_)).isSuccess, Assertion.equalTo(true))),
+    test("102230 fail")(assert(parse("012230",partTwoParser(0)(_)).isSuccess, Assertion.equalTo(false))),
+    test("123444 fail")(assert(parse("123444",partTwoParser(0)(_)).isSuccess, Assertion.equalTo(false))),
+    test("111122 ok")(assert(parse("111122",partTwoParser(0)(_)).isSuccess, Assertion.equalTo(true)))
   )
 
 })
